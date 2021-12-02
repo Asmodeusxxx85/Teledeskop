0269c0fe3dcb26f7e60085d1345ae603b8e3167c
John Preston <johnprestonmail@gmail.com>
2021-12-02 12:40:09 +0400

    Support pinned chats management.

--- Telegram/SourceFiles/window/window_peer_menu.cpp
+++ Telegram/SourceFiles/window/window_peer_menu.cpp
@@ -94,6 +94,8 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "styles/style_window.h" // st::windowMinWidth
 #include "styles/style_menu_icons.h"
 
+#include "tdb/tdb_tl_scheme.h"
+
 #include <QAction>
 #include <QtWidgets/QApplication>
 
@@ -150,6 +152,8 @@ const char kOptionViewProfileInChatsListContextMenu[]
 	= "view-profile-in-chats-list-context-menu";
 
 namespace {
+
+using namespace Tdb;
 
 constexpr auto kArchivedToastDuration = crl::time(5000);
 constexpr auto kMaxUnreadWithoutConfirmation = 1000;
@@ -392,6 +396,7 @@ void TogglePinnedThread(
 		return;
 	}
 
+#if 0 // mtp
 	owner->setChatPinned(entry, FilterId(), isPinned);
 	if (const auto history = entry->asHistory()) {
 		const auto flags = isPinned
@@ -427,6 +432,18 @@ void TogglePinnedThread(
 		//if (isPinned) {
 		//	controller->content()->dialogsToUp();
 		//}
+	}
+#endif
+	if (const auto history = thread->asHistory()) {
+		history->session().sender().request(TLtoggleChatIsPinned(
+			history->folder() ? tl_chatListArchive() : tl_chatListMain(),
+			peerToTdbChat(history->peer->id),
+			tl_bool(isPinned)
+		)).done([=] {
+			owner->notifyPinnedDialogsOrderUpdated();
+		}).send();
+	} else if (const auto topic = thread->asTopic()) {
+		// todo
 	}
 }
 
