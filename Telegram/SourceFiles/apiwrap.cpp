381ddf4df39ef39fa6e9047d8479940416312e52
John Preston <johnprestonmail@gmail.com>
2022-11-04 18:13:22 +0400

    Migrate (sort-of) chats filters to TDLib.

--- Telegram/SourceFiles/apiwrap.cpp
+++ Telegram/SourceFiles/apiwrap.cpp
@@ -211,12 +211,14 @@ ApiWrap::ApiWrap(not_null<Main::Session*> session)
 	crl::on_main(session, [=] {
 		// You can't use _session->lifetime() in the constructor,
 		// only queued, because it is not constructed yet.
+#if 0 // mtp
 		_session->data().chatsFilters().changed(
 		) | rpl::filter([=] {
 			return _session->data().chatsFilters().archiveNeeded();
 		}) | rpl::start_with_next([=] {
 			requestMoreDialogsIfNeeded();
 		}, _session->lifetime());
+#endif
 
 		setupSupportMode();
 
@@ -1116,6 +1118,7 @@ void ApiWrap::refreshDialogsLoadBlocked() {
 }
 
 void ApiWrap::requestMoreDialogsIfNeeded() {
+#if 0 // mtp
 	const auto dialogsReady = !_dialogsLoadState
 		|| _dialogsLoadState->listReceived;
 	if (_session->data().chatsFilters().loadNextExceptions(dialogsReady)) {
@@ -1130,6 +1133,13 @@ void ApiWrap::requestMoreDialogsIfNeeded() {
 		if (_session->data().chatsFilters().archiveNeeded()) {
 			requestMoreDialogs(folder);
 		}
+	}
+#endif
+	if (_dialogsLoadState && !_dialogsLoadState->listReceived) {
+		if (_dialogsLoadState->requestId) {
+			return;
+		}
+		requestDialogs(nullptr);
 	}
 	requestContacts();
 	_session->data().shortcutMessages().preloadShortcuts();
@@ -2510,7 +2520,9 @@ void ApiWrap::deleteHistory(
 			revoke);
 	}
 	if (!justClear) {
+#if 0 // mtp
 		_session->data().deleteConversationLocally(peer);
+#endif
 	} else if (history) {
 		history->clear(History::ClearType::ClearHistory);
 	}
