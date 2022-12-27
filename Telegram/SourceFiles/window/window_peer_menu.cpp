4809b02abd9a8f7e3bbc5fc791cf64470541deac
John Preston <johnprestonmail@gmail.com>
2022-12-27 14:45:22 +0400

    Use uniform "// later" and "// tdlib" comments.
    Now we have:
    // todo - needs to be done
    // mtp - can be removed later
    // later - can be improved later
    // tdlib - waiting for some improvements in the library

--- Telegram/SourceFiles/window/window_peer_menu.cpp
+++ Telegram/SourceFiles/window/window_peer_menu.cpp
@@ -465,7 +465,7 @@ void TogglePinnedThread(
 			owner->notifyPinnedDialogsOrderUpdated();
 		}).send();
 	} else if (const auto topic = thread->asTopic()) {
-		// later-todo pinned topics
+		// tdlib pinned topics
 	}
 }
 
