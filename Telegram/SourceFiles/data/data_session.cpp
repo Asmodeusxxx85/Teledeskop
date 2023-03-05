14c2f1054e415e9f15f2badc9aa77407326829cc
John Preston <johnprestonmail@gmail.com>
2023-03-05 21:01:57 +0400

    Remove setattributes(), add local DocumentData generation.

--- Telegram/SourceFiles/data/data_session.cpp
+++ Telegram/SourceFiles/data/data_session.cpp
@@ -3789,7 +3789,6 @@ not_null<DocumentData*> Session::processDocument(
 		return document(data.vid().v);
 	});
 }
-#endif
 
 not_null<DocumentData*> Session::document(
 		DocumentId id,
@@ -3821,7 +3820,6 @@ not_null<DocumentData*> Session::document(
 	return result;
 }
 
-#if 0 // mtp
 void Session::documentConvert(
 		not_null<DocumentData*> original,
 		const MTPDocument &data) {
@@ -3952,7 +3950,6 @@ void Session::documentApplyFields(
 		data.vdc_id().v,
 		data.vsize().v);
 }
-#endif
 
 void Session::documentApplyFields(
 		not_null<DocumentData*> document,
@@ -3988,6 +3985,7 @@ void Session::documentApplyFields(
 	}
 #endif
 }
+#endif
 
 void Session::documentFileIdUpdated(
 		not_null<DocumentData*> document,
@@ -4643,6 +4641,12 @@ not_null<Data::CloudImage*> Session::location(const LocationPoint &point) {
 			prepared)).first->second.get();
 }
 
+not_null<PhotoData*> Session::processPhoto(const PhotoLocalData &data) {
+	const auto result = photo(data.id);
+	result->setFromLocal(data);
+	return result;
+}
+
 not_null<PhotoData*> Session::processPhoto(const TLphoto &data) {
 	const auto result = photo(PhotoData::IdFromTdb(data));
 	result->setFromTdb(data);
@@ -4659,6 +4663,13 @@ not_null<PhotoData*> Session::processSmallPhoto(
 		const TLchatPhotoInfo &data) {
 	const auto result = photo(PhotoData::IdFromTdb(data));
 	result->setFromTdb(data);
+	return result;
+}
+
+not_null<DocumentData*> Session::processDocument(
+		const DocumentLocalData &data) {
+	const auto result = document(data.id);
+	result->setFromLocal(data);
 	return result;
 }
 
