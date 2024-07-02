telegramdesktop/tdx

Support upload pause and map thumbnail download.

Telegram/SourceFiles/storage/file_upload.cpp
@@ -782,12 +782,20 @@ void Uploader::cancelAll() {
 }
 
 void Uploader::pause(const FullMsgId &msgId) {
+	if (!_pausedId && msgId) {
+		_api->sender().request(TLsetNetworkType(
+			tl_networkTypeNone()
+		)).send();
+	}
 	_pausedId = msgId;
 }
 
 void Uploader::unpause() {
+	if (_pausedId) {
+		_api->sender().request(TLsetNetworkType(std::nullopt)).send();
+	}
 	_pausedId = FullMsgId();
-#if 0 // todo
+#if 0 // mtp
 	sendNext();
 #endif
 }

Asmodeusxxx85/Teledeskop

Telegram/SourceFiles/storage/file_upload.cpp
@@ -777,12 +777,20 @@ void Uploader::cancelAll() {
 }
 
 void Uploader::pause(FullMsgId itemId) {
+	if (!_pausedId && itemId) {
+		_api->sender().request(TLsetNetworkType(
+			tl_networkTypeNone()
+		)).send();
+	}
 	_pausedId = itemId;
 }
 
 void Uploader::unpause() {
+	if (_pausedId) {
+		_api->sender().request(TLsetNetworkType(std::nullopt)).send();
+	}
 	_pausedId = FullMsgId();
-#if 0 // todo
+#if 0 // mtp
 	maybeSend();
 #endif
 }