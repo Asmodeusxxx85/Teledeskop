telegramdesktop/tdx

Migrated draft support from MTP to TDLib.

Telegram/SourceFiles/apiwrap.cpp
@@ -2427,6 +2427,8 @@ void ApiWrap::saveDraftsToCloud() {
 			cloudDraft = history->createCloudDraft(topicRootId, nullptr);
 		}
 
+		auto &textWithTags = cloudDraft->textWithTags;
+#if 0 // goodToRemove
 		auto flags = MTPmessages_SaveDraft::Flags(0);
 		auto &textWithTags = cloudDraft->textWithTags;
 		if (cloudDraft->previewState != Data::PreviewState::Allowed) {
@@ -2445,8 +2447,73 @@ void ApiWrap::saveDraftsToCloud() {
 			_session,
 			TextUtilities::ConvertTextTagsToEntities(textWithTags.tags),
 			Api::ConvertOption::SkipLocal);
+#endif
+		auto tlDraft = [&]() -> std::optional<TLdraftMessage> {
+			if (!cloudDraft->msgId && textWithTags.text.isEmpty()) {
+				return std::nullopt;
+			}
+			using Preview = Data::PreviewState;
+			return tl_draftMessage(
+				tl_int53(cloudDraft->msgId.bare),
+				tl_int32(0), // Date.
+				tl_inputMessageText(
+					Api::FormattedTextToTdb(
+						_session,
+						TextWithEntities{
+							textWithTags.text,
+							TextUtilities::ConvertTextTagsToEntities(
+								textWithTags.tags)
+						},
+						Api::ConvertOption::SkipLocal),
+					tl_bool(cloudDraft->previewState != Preview::Allowed),
+					tl_bool(true)));
+		}();
+
+		const auto finishDraft = [=] {
+			sender().request(TLgetOption(
+				tl_string("unix_time")
+			)).done([=](const TLoptionValue &value) {
+				history->finishSavingCloudDraft(OptionValue<int64>(value));
+			}).send();
+		};
 
 		history->startSavingCloudDraft(topicRootId);
+
+		cloudDraft->saveRequestId = sender().request(TLsetChatDraftMessage(
+			peerToTdbChat(history->peer->id),
+			tl_int53(0), // Thread. // todo topics
+			std::move(tlDraft)
+		)).done([=](const TLok &, Tdb::RequestId requestId) {
+			finishDraft();
+
+			if (const auto cloudDraft = history->cloudDraft()) {
+				if (cloudDraft->saveRequestId == requestId) {
+					cloudDraft->saveRequestId = 0;
+					history->draftSavedToCloud();
+				}
+			}
+			auto i = _draftsSaveRequestIds.find(history);
+			if (i != _draftsSaveRequestIds.cend()
+				&& i->second == requestId) {
+				_draftsSaveRequestIds.erase(history);
+				checkQuitPreventFinished();
+			}
+		}).fail([=](const Tdb::Error &error, Tdb::RequestId requestId) {
+			finishDraft();
+
+			if (const auto cloudDraft = history->cloudDraft()) {
+				if (cloudDraft->saveRequestId == requestId) {
+					history->clearCloudDraft();
+				}
+			}
+			auto i = _draftsSaveRequestIds.find(history);
+			if (i != _draftsSaveRequestIds.cend()
+				&& i->second == requestId) {
+				_draftsSaveRequestIds.erase(history);
+				checkQuitPreventFinished();
+			}
+		}).send();
+#if 0 // goodToRemove
 		cloudDraft->saveRequestId = request(MTPmessages_SaveDraft(
 			MTP_flags(flags),
 			MTP_int(cloudDraft->msgId),
@@ -2488,6 +2555,7 @@ void ApiWrap::saveDraftsToCloud() {
 				checkQuitPreventFinished();
 			}
 		}).send();
+#endif
 
 		i->second = cloudDraft->saveRequestId;
 		++i;

Asmodeusxxx85/Teledeskop

Telegram/SourceFiles/apiwrap.cpp
@@ -2434,6 +2434,8 @@ void ApiWrap::saveDraftsToCloud() {
 			cloudDraft = history->createCloudDraft(topicRootId, nullptr);
 		}
 
+		auto &textWithTags = cloudDraft->textWithTags;
+#if 0 // goodToRemove
 		auto flags = MTPmessages_SaveDraft::Flags(0);
 		auto &textWithTags = cloudDraft->textWithTags;
 		if (cloudDraft->webpage.removed) {
@@ -2451,8 +2453,69 @@ void ApiWrap::saveDraftsToCloud() {
 			_session,
 			TextUtilities::ConvertTextTagsToEntities(textWithTags.tags),
 			Api::ConvertOption::SkipLocal);
+#endif
+		auto tlDraft = [&]() -> std::optional<TLdraftMessage> {
+			if (!cloudDraft->reply && textWithTags.text.isEmpty()) {
+				return std::nullopt;
+			}
+			return tl_draftMessage(
+				Api::MessageReplyTo(history, cloudDraft->reply),
+				tl_int32(0), // Date.
+				tl_inputMessageText(
+					Api::FormattedTextToTdb(TextWithEntities{
+						textWithTags.text,
+						TextUtilities::ConvertTextTagsToEntities(
+							textWithTags.tags)
+					}),
+					Data::LinkPreviewOptions(cloudDraft->webpage),
+					tl_bool(true)));
+		}();
+
+		const auto finishDraft = [=] {
+			sender().request(TLgetOption(
+				tl_string("unix_time")
+			)).done([=](const TLoptionValue &value) {
+				history->finishSavingCloudDraft(OptionValue<int64>(value));
+			}).send();
+		};
 
 		history->startSavingCloudDraft(topicRootId);
+
+		cloudDraft->saveRequestId = sender().request(TLsetChatDraftMessage(
+			peerToTdbChat(history->peer->id),
+			tl_int53(0), // Thread. // todo topics
+			std::move(tlDraft)
+		)).done([=](const TLok &, Tdb::RequestId requestId) {
+			finishDraft();
+
+			if (const auto cloudDraft = history->cloudDraft()) {
+				if (cloudDraft->saveRequestId == requestId) {
+					cloudDraft->saveRequestId = 0;
+					history->draftSavedToCloud();
+				}
+			}
+			auto i = _draftsSaveRequestIds.find(history);
+			if (i != _draftsSaveRequestIds.cend()
+				&& i->second == requestId) {
+				_draftsSaveRequestIds.erase(history);
+				checkQuitPreventFinished();
+			}
+		}).fail([=](const Tdb::Error &error, Tdb::RequestId requestId) {
+			finishDraft();
+
+			if (const auto cloudDraft = history->cloudDraft()) {
+				if (cloudDraft->saveRequestId == requestId) {
+					history->clearCloudDraft();
+				}
+			}
+			auto i = _draftsSaveRequestIds.find(history);
+			if (i != _draftsSaveRequestIds.cend()
+				&& i->second == requestId) {
+				_draftsSaveRequestIds.erase(history);
+				checkQuitPreventFinished();
+			}
+		}).send();
+#if 0 // goodToRemove
 		cloudDraft->saveRequestId = request(MTPmessages_SaveDraft(
 			MTP_flags(flags),
 			ReplyToForMTP(history, cloudDraft->reply),
@@ -2496,6 +2559,7 @@ void ApiWrap::saveDraftsToCloud() {
 				checkQuitPreventFinished();
 			}
 		}).send();
+#endif
 
 		i->second = cloudDraft->saveRequestId;
 		++i;