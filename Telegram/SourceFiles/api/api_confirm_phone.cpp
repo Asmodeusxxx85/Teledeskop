telegramdesktop/tdx

Migrated ConfirmPhoneBox from MTP to TDLib.

Telegram/SourceFiles/api/api_confirm_phone.cpp
@@ -19,7 +19,10 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 namespace Api {
 
 ConfirmPhone::ConfirmPhone(not_null<ApiWrap*> api)
+#if 0 // goodToRemove
 : _api(&api->instance()) {
+#endif
+: _api(&api->sender()) {
 }
 
 void ConfirmPhone::resolve(
@@ -29,6 +32,7 @@ void ConfirmPhone::resolve(
 	if (_sendRequestId) {
 		return;
 	}
+#if 0 // goodToRemove
 	_sendRequestId = _api.request(MTPaccount_SendConfirmPhoneCode(
 		MTP_string(hash),
 		MTP_codeSettings(
@@ -158,6 +162,99 @@ void ConfirmPhone::resolve(
 			Ui::MakeInformBox(errorText),
 			Ui::LayerOption::CloseOther);
 	}).handleFloodErrors().send();
+#endif
+	_sendRequestId = _api.request(Tdb::TLsendPhoneNumberConfirmationCode(
+		Tdb::tl_string(hash),
+		Tdb::tl_string(phone),
+		std::nullopt
+	)).done([=](const Tdb::TLDauthenticationCodeInfo &data) {
+		_sendRequestId = 0;
+
+		const auto sentCodeLength = data.vtype().match([](
+				const Tdb::TLDauthenticationCodeTypeTelegramMessage &data) {
+			LOG(("Error: should not be in-app code!"));
+			return 0;
+		}, [](const Tdb::TLDauthenticationCodeTypeSms &data) {
+			return data.vlength().v;
+		}, [](const Tdb::TLDauthenticationCodeTypeCall &data) {
+			return data.vlength().v;
+		}, [](const Tdb::TLDauthenticationCodeTypeFlashCall &data) {
+			LOG(("Error: should not be flashcall!"));
+			return 0;
+		});
+		const auto timeout = [&]() -> std::optional<int> {
+			if (const auto next = data.vnext_type()) {
+				using TypeCall = Tdb::TLDauthenticationCodeTypeCall;
+				return next->match([&](const TypeCall &) {
+					return std::make_optional<int>(data.vtimeout().v);
+				}, [](const auto &) {
+					return std::optional<int>(std::nullopt);
+				});
+			}
+			return std::nullopt;
+		}();
+		auto box = Box<Ui::ConfirmPhoneBox>(
+			phone,
+			sentCodeLength,
+			timeout);
+		const auto boxWeak = Ui::MakeWeak(box.data());
+		box->resendRequests(
+		) | rpl::start_with_next([=] {
+			_api.request(Tdb::TLresendPhoneNumberConfirmationCode(
+			)).done([=] {
+				if (boxWeak) {
+					boxWeak->callDone();
+				}
+			}).send();
+		}, box->lifetime());
+		box->checkRequests(
+		) | rpl::start_with_next([=](const QString &code) {
+			if (_checkRequestId) {
+				return;
+			}
+			_checkRequestId = _api.request(
+				Tdb::TLcheckPhoneNumberConfirmationCode(Tdb::tl_string(code))
+			).done([=] {
+				_checkRequestId = 0;
+				controller->show(
+					Box<Ui::InformBox>(
+						tr::lng_confirm_phone_success(
+							tr::now,
+							lt_phone,
+							Ui::FormatPhone(phone))),
+					Ui::LayerOption::CloseOther);
+			}).fail([=](const Tdb::Error &error) {
+				_checkRequestId = 0;
+				if (!boxWeak) {
+					return;
+				}
+
+				const auto errorText = Tdb::IsFloodError(error)
+					? tr::lng_flood_error(tr::now)
+					: (error.message == (u"PHONE_CODE_EMPTY"_q)
+						|| error.message == (u"PHONE_CODE_INVALID"_q))
+					? tr::lng_bad_code(tr::now)
+					: Lang::Hard::ServerError();
+				boxWeak->showServerError(errorText);
+			}).send();
+			// doLater handleFloodErrors.
+		}, box->lifetime());
+
+		controller->show(std::move(box), Ui::LayerOption::CloseOther);
+	}).fail([=](const Tdb::Error &error) {
+		_sendRequestId = 0;
+		_checkRequestId = 0;
+
+		const auto errorText = Tdb::IsFloodError(error)
+			? tr::lng_flood_error(tr::now)
+			: (error.code == 400)
+			? tr::lng_confirm_phone_link_invalid(tr::now)
+			: Lang::Hard::ServerError();
+		controller->show(
+			Box<Ui::InformBox>(errorText),
+			Ui::LayerOption::CloseOther);
+	}).send();
+	// doLater handleFloodErrors.
 }
 
 } // namespace Api

Asmodeusxxx85/Teledeskop

Telegram/SourceFiles/api/api_confirm_phone.cpp
@@ -29,6 +29,7 @@ void ConfirmPhone::resolve(
 	if (_sendRequestId) {
 		return;
 	}
+#if 0 // goodToRemove
 	_sendRequestId = _api.request(MTPaccount_SendConfirmPhoneCode(
 		MTP_string(hash),
 		MTP_codeSettings(
@@ -164,6 +165,111 @@ void ConfirmPhone::resolve(
 			Ui::MakeInformBox(errorText),
 			Ui::LayerOption::CloseOther);
 	}).handleFloodErrors().send();
+#endif
+	_sendRequestId = _api.request(Tdb::TLsendPhoneNumberCode(
+		Tdb::tl_string(phone),
+		std::nullopt,
+		Tdb::tl_phoneNumberCodeTypeConfirmOwnership(Tdb::tl_string(hash))
+	)).done([=](const Tdb::TLDauthenticationCodeInfo &data) {
+		_sendRequestId = 0;
+
+		const auto sentCodeLength = data.vtype().match([](
+				const Tdb::TLDauthenticationCodeTypeTelegramMessage &data) {
+			LOG(("Error: should not be in-app code!"));
+			return 0;
+		}, [](const Tdb::TLDauthenticationCodeTypeSms &data) {
+			return data.vlength().v;
+		}, [](const Tdb::TLDauthenticationCodeTypeCall &data) {
+			return data.vlength().v;
+		}, [](const Tdb::TLDauthenticationCodeTypeFragment &data) {
+			return data.vlength().v;
+		}, [](const Tdb::TLDauthenticationCodeTypeSmsWord &data) {
+			LOG(("Error: should not be smsword!"));
+			return 0;
+		}, [](const Tdb::TLDauthenticationCodeTypeSmsPhrase &data) {
+			LOG(("Error: should not be smsphrase!"));
+			return 0;
+		}, [](const Tdb::TLDauthenticationCodeTypeMissedCall &data) {
+			LOG(("Error: should not be missedcall!"));
+			return 0;
+		}, [](const Tdb::TLDauthenticationCodeTypeFlashCall &data) {
+			LOG(("Error: should not be flashcall!"));
+			return 0;
+		});
+		const auto timeout = [&]() -> std::optional<int> {
+			if (const auto next = data.vnext_type()) {
+				using TypeCall = Tdb::TLDauthenticationCodeTypeCall;
+				return next->match([&](const TypeCall &) {
+					return std::make_optional<int>(data.vtimeout().v);
+				}, [](const auto &) {
+					return std::optional<int>(std::nullopt);
+				});
+			}
+			return std::nullopt;
+		}();
+		auto box = Box<Ui::ConfirmPhoneBox>(
+			phone,
+			sentCodeLength,
+			timeout);
+		const auto boxWeak = Ui::MakeWeak(box.data());
+		box->resendRequests(
+		) | rpl::start_with_next([=] {
+			_api.request(Tdb::TLresendPhoneNumberCode(
+				Tdb::tl_resendCodeReasonUserRequest()
+			)).done([=] {
+				if (boxWeak) {
+					boxWeak->callDone();
+				}
+			}).send();
+		}, box->lifetime());
+		box->checkRequests(
+		) | rpl::start_with_next([=](const QString &code) {
+			if (_checkRequestId) {
+				return;
+			}
+			_checkRequestId = _api.request(
+				Tdb::TLcheckPhoneNumberCode(Tdb::tl_string(code))
+			).done([=] {
+				_checkRequestId = 0;
+				controller->show(
+					Box<Ui::InformBox>(
+						tr::lng_confirm_phone_success(
+							tr::now,
+							lt_phone,
+							Ui::FormatPhone(phone))),
+					Ui::LayerOption::CloseOther);
+			}).fail([=](const Tdb::Error &error) {
+				_checkRequestId = 0;
+				if (!boxWeak) {
+					return;
+				}
+
+				const auto errorText = Tdb::IsFloodError(error)
+					? tr::lng_flood_error(tr::now)
+					: (error.message == (u"PHONE_CODE_EMPTY"_q)
+						|| error.message == (u"PHONE_CODE_INVALID"_q))
+					? tr::lng_bad_code(tr::now)
+					: Lang::Hard::ServerError();
+				boxWeak->showServerError(errorText);
+			}).send();
+			// doLater handleFloodErrors.
+		}, box->lifetime());
+
+		controller->show(std::move(box), Ui::LayerOption::CloseOther);
+	}).fail([=](const Tdb::Error &error) {
+		_sendRequestId = 0;
+		_checkRequestId = 0;
+
+		const auto errorText = Tdb::IsFloodError(error)
+			? tr::lng_flood_error(tr::now)
+			: (error.code == 400)
+			? tr::lng_confirm_phone_link_invalid(tr::now)
+			: Lang::Hard::ServerError();
+		controller->show(
+			Box<Ui::InformBox>(errorText),
+			Ui::LayerOption::CloseOther);
+	}).send();
+	// doLater handleFloodErrors.
 }
 
 } // namespace Api